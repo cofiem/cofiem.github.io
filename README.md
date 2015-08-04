345cd2b9836955e5670500e65582f29000a0f8a4
Mark Cottman-Fields <cofiem@gmail.com>
2015-08-05 00:44:55 +1000

    updated the way tags are generated

--- README.md
+++ README.md
@@ -9,16 +9,30 @@ Uses the [Lagom theme](https://github.com/swanson/lagom) by [@swanson](https://g
 New posts are created in `/_posts`, named in the format `yyyy-mm-dd-title`, where title is an alphanumeric dash-separated string.
 Drafts can be created in `/_drafts`, using the same file name format.
 
-TO preview the website, which includes the ability to generate pages as they are edited, run
+The website can be previewed and regenerated as the pages are edited. First, make some changes to the `/_config.yml` file:
 
-    jekyll serve --safe
+    - safe: false
+    
+Then add `tag/*` to the `exclude` array.
+
+Then start the jekyll server:
+
+    bundle exec jekyll serve --safe
 
 The safe flag ensures it replicates the way Github pages works.
 
-This blog uses a custom generator to add support for tags. Before commiting and pushing in git, run
+This blog uses a custom generator to add support for tags. 
 
-    jekyll build
-    
+Before uploading, build the entire site, including running the custom generator:
+
+    bundle exec jekyll build
+
 This will ensure the files for tags are generated and included in the live site.
 
-To be extra correct, set `safe` to `true` in `_config.yml` before commiting.
+## Deploying to live site
+
+First, undo the changes to the `/config.yml` file:
+
+    - safe: true
+
+Remove `tag/*` from the `exclude` array.
--- _plugins/generate_tags.rb
+++ _plugins/generate_tags.rb
@@ -33,34 +33,88 @@ permalink: /tag/#{tag_slug}/
     end
   end
 
-  class CategoryPageGenerator < Generator
+  class TagPageGenerator < Generator
     safe false
 
     # @see http://www.minddust.com/post/tags-and-categories-on-github-pages/
     def generate(site)
 
-      just_tags = site.tags.select { |i| i[0] }.sort { |a, b| a[0] <=> b[0] }
+      site_tags = site.tags.select { |i| i[0] }.sort { |a, b| format_tag_slug(a[0]) <=> format_tag_slug(b[0]) }
+      site_posts = site.posts
 
-      tag_hash = {}
-      tag_name_hash = {}
-      tag_array = []
-      just_tags.each do |tag|
-        tag_hash[tag[0].parameterize] = tag[0]
-        tag_name_hash[tag[0]] = tag[0].parameterize
-        tag_array.push({'slug' => tag[0].parameterize, 'name' => tag[0]})
+      # gather tag info
+      tags_slug_keys = {}
+      site_tags.each do |tag_info|
+        tag_slug, tag_name = tag_info_from_info(tag_info)
+        posts = tag_info[1]
+        post_count = posts.size
+
+        if tags_slug_keys.include?(tag_slug)
+          tags_slug_keys[tag_slug]['post_count'] += post_count
+          tags_slug_keys[tag_slug]['posts'] += posts.map { |p| format_post(p) }
+        else
+          tag_info = {
+              'slug' => tag_slug,
+              'name' => tag_name,
+              'post_count' => post_count,
+              'posts' => posts.map { |p| format_post(p) }}
+
+          tags_slug_keys[tag_slug] = tag_info
+        end
       end
 
-      # write yaml file
-      File.open(File.join('_data', 'tags.yml'), 'w') { |f| f.write tag_array.to_yaml }
-      File.open(File.join('_data', 'tagshash.yml'), 'w') { |f| f.write tag_hash.to_yaml }
-      File.open(File.join('_data', 'tagsnamehash.yml'), 'w') { |f| f.write tag_name_hash.to_yaml }
+      # collect tag names and slugs for each post
+      all_posts = {}
+      site_posts.each do |post|
+        posts_tags = {}
+        post.data['tags'].each do |raw_tag_name|
+          tag_slug, tag_name = tag_info_from_name(raw_tag_name)
+          posts_tags[tag_slug] = tag_name
+        end
+        all_posts[post.id] = format_post(post)
+        all_posts[post.id]['tags'] = posts_tags
+      end
 
+      # write yaml files
+      write_yaml('custom_tags_slug_keys', tags_slug_keys)
+      write_yaml('custom_post_tags', all_posts)
+
+      # create individual tag pages
       dir = site.config['tag_dir'] || 'tag'
       FileUtils.mkpath File.join(site.source, dir)
       site.tags.each do |tag|
         site.pages << TagPage.new(site, site.source, dir, tag[0])
       end
     end
+
+    private
+
+    def tag_info_from_name(tag_name)
+      tag_slug = format_tag_slug(tag_name)
+      tag_name = format_tag_name(tag_name)
+      [tag_slug, tag_name]
+    end
+
+    def format_tag_slug(tag_name)
+      tag_name.to_s.parameterize.downcase
+    end
+
+    def format_tag_name(tag_name)
+      tag_name.to_s.downcase
+    end
+
+    def tag_info_from_info(tag_info)
+      tag_info_from_name(tag_info[0])
+    end
+
+    def write_yaml(file_name, content)
+      File.open(File.join('_data', "#{file_name}.yml"), 'w') { |f| f.write content.to_yaml }
+    end
+
+    def format_post(post)
+      {'url' => post.url, 'date' => post.date, 'title' => post.title}
+    end
+
   end
 
 end
